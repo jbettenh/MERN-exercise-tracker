085a3633479eb66cac4ec1ca7a966b05c426ecda
jbettenh <21023218+jbettenh@users.noreply.github.com>
2019-12-29 11:49:54 +0100

    Connect creating user and exercise components to backend, with axios

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+
 
 export default class CreateExercises extends Component {
   constructor(props) {
@@ -23,10 +25,18 @@ export default class CreateExercises extends Component {
 
   componentDidMount() {
      // automatically called before anything loads to page
-     this.setState({
-       users: ['test user'],
-       username: 'test user'
-     })
+     axios.get('http://localhost:5000/users/')
+      .then(response => {
+        if (response.data.length > 0) {
+          this.setState({
+            users: response.data.map(user => user.username),
+            username: response.data[0].username
+          })
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
   onChangeUsername(e){
@@ -64,6 +74,9 @@ onSubmit(e) {
   }
 
   console.log(exercise);
+
+  axios.post('http://localhost:5000/exercises/add', exercise)
+      .then(res => console.log(res.data));
 
   window.location = '/';
 }
