edd9755056b4d09fb4cdcb72f99b1898d4c74f72
jbettenh <21023218+jbettenh@users.noreply.github.com>
2020-01-12 10:20:46 +0100

    added functions

--- installer.sh
+++ installer.sh
@@ -1,27 +1,38 @@
 #!/bin/sh
-# install_dependencies.sh will install the necessary packages
+# installer.sh will install the necessary packages
+
+main () {
+  rpi
+  config_env
+  install_be
+  install_fe
+  
+}
+
+rpi() {
 
 # Version of Raspbian
 apt-get install lsb_release
 lsb_release -a
-
-  
+ 
 # Run Update & Upgrade
 echo "\n"
 echo "  Running apt-get update & upgrade \n"
 
 #apt-get update && apt-get upgrade -y
-
-echo "\n"
-echo "\n"
-echo "\n"
 printf "  Current version of npm: $( npm -v)\n"
 npm install -g npm
 printf  "  New version of npm: $( npm -v)\n"
-echo "\n"
-echo "\n"
+}
 
+config_env() {
+printf "  Configuring Environment.........."
+# read -p $'\e[33mWhat\'s your MongoDB credentials? \e[39m' -r mongodb_connstr
+#git pull origin master
+}
 
+install_be() {
+echo "================================================="
 printf "  Deploying Backend packages.........."
 echo "  Installing express.js..."
 npm install express
@@ -49,11 +60,11 @@ npm install nodemon
 echo "\n"
 echo "\n"
 echo "\n"
+}
 
-
+install_fe() {
+echo "================================================="
 printf "  Deploying Frontend package.........."
+}
 
-printf "  Configuring Environment.........."
-echo
-read -p $'\e[33mWhat\'s your MongoDB credentials? \e[39m' -r mongodb_connstr
-
+main
